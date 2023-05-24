c72e31d0146779798328df4dbc4dfc581dccfcf2
larrytu327 <larrytu327@gmail.com>
2023-05-23 19:59:21 -0700

    worked on add button to submit recommendations from businesses index

--- travelrecs/src/pages/BusinessesIndex.js
+++ travelrecs/src/pages/BusinessesIndex.js
@@ -4,6 +4,20 @@ import { useState, useEffect } from "react";
 function BusinessesIndex() {
     const [businesses, setBusinesses] = useState([]);
 
+    const [businessListAdd, setBusinessListAdd] = useState({
+        name: "",
+        rating: "",
+        // _id: "",
+        // review_count: "",
+        // location: {
+        //     address1: "",
+        //     city: "",
+        //     state: "",
+        //     zip_code: ""
+        // },
+        // url: ""
+    })
+
     async function getBusinesses() {
         try {
             let myBusinesses = await fetch("http://localhost:4000/businesses");
@@ -25,9 +39,14 @@ function BusinessesIndex() {
                     return(
                         <div key={idx}>
                                 <Link to={`/businesses/${business._id}`}>
-                                    <h2>Name: {business.name}</h2>
+                                    <h2>{business.name}</h2>
                                 </Link>
-                                <h3>Rating: {business.rating}</h3>
+                                <h3>Yelp Rating ({business.review_count} reviews): {business.rating} ⭐</h3>
+                                <h3>{business.location.address1}, {business.location.city}, {business.location.state} {business.location.zip_code}</h3>
+                                <Link to={business.url}>Yelp Page</Link>
+                                <br></br>
+                                <br></br>
+                                <button type="button">Add to List</button>
                                 <hr />
                         </div>
                     )
@@ -35,8 +54,55 @@ function BusinessesIndex() {
             </>
         )
     }
+
+    // async function addToList(business) {
+    //     try {
+    //         console.log("add");
+    //         await fetch('http://localhost:4000/recommendations', {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json"
+    //             },
+    //             body: JSON.stringify(business)
+    //         })
+    //     } catch(err) {
+    //         console.log(err);
+    //     }
+    // }
+
+    function handleChange(e) {
+        console.log(e.target);
+        setBusinessListAdd((previousFormState) => ({
+            ...previousFormState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    async function handleSubmit(e, business) {
+        try {
+            e.preventDefault();
+            await fetch('http://localhost:4000/recommendations', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(businessListAdd)
+            })
+            e.target.reset();
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
     return (
         <>
+            <form onSubmit={handleSubmit}>
+                <label>Name: </label>
+                <input type="text" name="name" onChange={handleChange}/>
+                <label>Rating: </label>
+                <input type="text" name="rating" onChange={handleChange}/>
+                <button>Submit</button>
+            </form>
             {businesses.length ? loaded(businesses) : <h2>Loading...</h2>}
         </>
     );
