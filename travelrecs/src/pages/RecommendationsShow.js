701b61b6a9c026de60669219c26f17d5d9859366
raymondborel <rborel852@gmail.com>
2023-05-24 18:44:53 -0700

    finished recs Show page

--- travelrecs/src/pages/RecommendationsShow.js
+++ travelrecs/src/pages/RecommendationsShow.js
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-function RecommendationsShow () {
-    const { recommendationId } = useParams();
-    console.log(recommendationId)
+function RecommendationsShow() {
+  const [recommendation, setRecommendation] = useState(null);
+  const { recommendationId } = useParams();
+  console.log(recommendationId);
+
+  async function getRecommendation() {
+    try {
+      let myRecommendation = await fetch(
+        `http://localhost:400/recommendation/${recommendationId}`
+      );
+      myRecommendation = await myRecommendation.json();
+      setRecommendation(myRecommendation);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function recommendationLoaded() {
     return (
-        <h2> This is your recommended restaurant's Show Page </h2>
+        <>
+        <h2>Name: {recommendation.title}</h2>
+        <h2>Name: {recommendation.rating}</h2>
+        </>
     )
+  }
+
+
+  useEffect(() => {
+    getRecommendation();
+  }, []);
+
+  return (
+    <>
+        {recommendation ? recommendationLoaded() : <h2>Loading...</h2>}
+    </>
+  )
 }
 
 export default RecommendationsShow;
